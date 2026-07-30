cb1a0f9db78bd06019252b2e757c10503920abc3
InesBonkindoMbowuka <ines.bonkindo.mbowuka@student.ehb.be>
2026-07-30 14:23:17 +0200

    implement streetlight api

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const connectDatabase = require("./config/database");
 const pedestrianRoutes = require("./routes/pedestrianRoutes");
+const streetlightRoutes = require("./routes/streetlightRoutes");
 
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 connectDatabase();
 
 app.use("/api/pedestrians", pedestrianRoutes); 
+app.use("/api/streetlights", streetlightRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
